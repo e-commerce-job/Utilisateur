2fc3ad1b6a26f4c160c6800adb9938eefada150d
aldjiaoudjane <aldjia1999al@gmail.com>
2025-08-04 14:41:35 +0200

    ajout de docker compose du microservice

---
+++ src/main/java/com/ecommerce/utilisateur/controller/userController.java
@@ -0,0 +1,12 @@
+package com.ecommerce.utilisateur.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class userController {
+    @GetMapping("/")
+    public String hello() {
+        return "Bonjour, Aldjia !";
+    }
+}
